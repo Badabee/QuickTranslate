7f13af8ef5dbfccbda713534fdf441b32d59b759
Badabee <badabee440@gmail.com>
2020-08-18 18:46:16 +0100

    events

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const text = document.getElementById("text");
 const document = document.getElementById("document");
+const itemA = document.getElementById("item1");
 
+itemA.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        
+    }
+});
