328f691d345c6918a34ada92d4bb90c3298fe719
Badabee <badabee440@gmail.com>
2020-08-20 15:38:44 +0100

    update

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const text = document.getElementById("text");
 const document = document.getElementById("document");
 const itemA = document.getElementById("item1");
+const awesome = document.getElementById("awesome");
+const formality = document.querySelector("form");
 
 itemA.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
+    }
+});
 
-        formality.reset();
-        
+awesome.addEventListener("onclick", function(event) {
+    if (event.key === "onclick") {
+        event.preventDefault();
+
+        formality.reset()
     }
 });
 
