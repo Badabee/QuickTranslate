1d481b1e1d5752725ca1ee95bc3f113b296b8f08
Badabee <badabee440@gmail.com>
2020-08-21 17:00:45 +0100

    progress

--- main.js
+++ main.js
@@ -7,8 +7,33 @@ getLanguages().then((languages) => {
       select.innerHTML = "<option value=${value}>value</option>"
     });
   })
+
   const translationForm = document.forms.translation;
 
   const query = translationForm.query.value;
-  const target = translationForm.source.value
+  const target = translationForm.source.value;
+
+
+  const translate = async (query, source, target) =>  {
+    const res = await fetch(`https://translation.googleapis.com/language/translate/v2?q=${query}&target=${target}&source=${sourc}&format=text&key=`);
+    const data = await res.json();
+
+    return data.data.translate.translatedText;
+  }
+
+  const getLanguages = async (data) => {
+    const res = await fetch(`https://translation.googleapis.com/language/translate/v2/languages?key=`);
+    const data = await res.json();
+
+    return data.data.languages.map(langObject => langObject.language);
+  }
+
+  translationForm.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const translation = await translate(query,....);
+    // Query textarea first
+    textareaElement.innerText(translation);
+
+  })
   
